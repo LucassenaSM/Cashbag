89086aee0520e51b2d18e51f02b90605e99b5174
Lucas Sena <42158020+seninhaZIKA@users.noreply.github.com>
2022-08-28 21:26:00 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function calcularCDB() {
     }
   }
 }
-btCalcular.addEventListener("click", calcularCDB);
+//btCalcular.addEventListener("click", calcularCDB);
 //------------------------------------------------------------------------------------------------------------------------------//
 /* Criar Conta*/
 
@@ -45,7 +45,38 @@ function ContaCriar() {
       alert("O campo de Email está vazio!")
       inEmail.focus();
     } else {
-      
+      btConta.textContent = "Entrar";
     }
   }
 }
+btContaCriar.addEventListener("click", ContaCriar)
+//------------------------------------------------------------------------------------------------------------------------------//
+//entrar
+var inNome1 = document.getElementById("inNome1");
+var inEmail1 = document.getElementById("inEmail1");
+var btConta = document.getElementById("perfil")
+var btContaCriar1 = document.getElementById("btContaCriar1")
+
+function entrar() {
+document.getElementById("login").style.display = "flex"
+document.getElementById("blur").style.filter= "blur(1vh)"
+}
+btConta.addEventListener("click", entrar)
+function sair(){
+  var nome1 = inNome1.value;
+var email1 = inEmail1.value;
+if (nome1 = "Lucas" && email1 == "Lucas") {
+  btConta.textContent = "Sua Conta";
+  btConta.style.textAlign = "center"; 
+  document.getElementById("menu").style.border = "#0BD863 solid 3px";
+  document.getElementById("menu").style.borderRadius = "2vh";
+  document.getElementById("menu").style.borderRight = "0vh";
+  document.getElementById("menu").style.borderLeft = "0vh";
+  document.getElementById("menu").style.transition = "0.2s";
+  document.getElementById("menu2").style.display = "block";
+  document.getElementById("login").style.display = "none";
+  document.getElementById("blur").style.filter= "blur(0vh)"
+} 
+
+}
+btContaCriar1.addEventListener("click", sair);
